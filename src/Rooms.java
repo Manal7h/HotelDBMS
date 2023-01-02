26dbf1fd381ae91db8cdea2a335c8c1f8f0c56c6
manal7h <manal.alsenani22@gmail.com>
2023-01-02 03:37:42 -0800

    Entering the data

--- src/Rooms.java
+++ src/Rooms.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Rooms {
 
@@ -11,5 +12,24 @@ public class Rooms {
 	    String sql = "CREATE TABLE Room_Type" + "(id INTEGER PRIMARY KEY ," + "room_type_id INTEGER FOREIGN KEY REFERENCES Rooms(id)," + 
 	    	    " hotel_id INTEGER FOREIGN KEY REFERENCES Hotels(id) , "+ " created_date date NOT NULL ," + " updated_date date," + "is_Active BIT NOT NULL )";
 		
+	    Scanner scanner = new Scanner(System.in);
+	    System.out.print("Enter id :");
+	    Integer id = scanner.nextInt();
+	    
+	    System.out.print("Enter room type id :");
+	    Integer room_type_id = scanner.nextInt();
+	    
+	    System.out.print("Enter hotel id :");
+	    Integer hotel_id = scanner.nextInt();
+	    
+	    System.out.print("Created date :");
+	    String created_date = scanner.next();
+	    
+	    System.out.print("Updated date :");
+	    String updated_date = scanner.next();
+	    
+	    System.out.print("Is Active date :");
+	    String is_Active = scanner.next();
+	    
 	}
 }
