8fe6438a83818e8915d42edab9d7191a45b8348b
manal7h <manal.alsenani22@gmail.com>
2023-01-02 03:20:50 -0800

    create rooms table

---
+++ src/Rooms.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+
+public class Rooms {
+
+	
+	public static void RoomsTable() throws IOException{
+		String url = "jdbc:sqlserver://localhost:1433;databaseName=HotelDBMS;encrypt=true;trustServerCertificate=true";
+		String user = "sa";
+	    String pass = "root";
+	    
+		
+		
+	}
+}
