25240d1234d85918cb79c297c391c9c8b13b1f13
manal7h <manal.alsenani22@gmail.com>
2023-01-02 01:43:59 -0800

    add main mune

--- src/HotelsTask.java
+++ src/HotelsTask.java
@@ -1,8 +1,22 @@
 
 public class HotelsTask {
+	
+	static void MainMune(){
+		System.out.print("Please Choice one Option:");
+		System.out.print("1. Create Hotels Table");
+		System.out.print("2. Create Room Type Table");
+		System.out.print("3. Create Room Table");
+		System.out.print("4. Create Guests Table");
+		System.out.print("5. Create Employee Type Table");
+		System.out.print("6. Create Employee Table");
+		System.out.print("7. Exit");
+		
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 
 	}
 
