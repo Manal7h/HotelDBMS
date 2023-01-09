18ae1ad9885eade866453930d0b8c904962354b2
manal7h <manal.alsenani22@gmail.com>
2023-01-08 23:53:38 -0800

    hotel must atleast have 10 rooms

---
+++ src/HotelManagement.java
@@ -0,0 +1,15 @@
+
+public class HotelManagement {
+
+	public boolean HotelManagement(String chars) {
+		
+		String myStr = "Hello";
+		System.out.println(myStr.endsWith("e"));   // false
+		
+		
+		return false;
+		// TODO Auto-generated method stub
+
+	}
+
+}
